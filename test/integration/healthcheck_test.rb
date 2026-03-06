268e71e3ff41115be1741ea637102b1794f1ae9c
Mauricio Junior <mauriciojs.junior@gmail.com>
2026-03-05 21:57:25 -0300

    test(01-02): update healthcheck route expectations

--- test/integration/healthcheck_test.rb
+++ test/integration/healthcheck_test.rb
@@ -7,12 +7,13 @@ class HealthcheckTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "application exposes only healthcheck route" do
+  test "application exposes healthcheck and signup routes" do
     app_routes = Rails.application.routes.routes.select do |route|
       route.defaults[:controller].present? && !route.path.spec.to_s.start_with?("/rails/")
     end
 
-    assert_equal 1, app_routes.count
+    assert_equal 2, app_routes.count
     assert_routing "/up", controller: "rails/health", action: "show"
+    assert_routing({ method: "post", path: "/auth/signup" }, controller: "auth/signups", action: "create")
   end
 end
